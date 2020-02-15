e6365c523e6df298b4751ca863b7511890f9f3ca
yamamaura_tatusya <t.yamamura@gemcook.com>
2020-02-15 20:27:41 +0900

    [add] PostのGet（All）APIを作成しました。

--- handler/posts.go
+++ handler/posts.go
@@ -46,3 +46,17 @@ func GetPost(c *gin.Context) {
 	}
 	c.JSON(http.StatusCreated, post)
 }
+
+// GetPostAll は投稿の新規登録
+func GetPostAll(c *gin.Context) {
+
+	servicer := c.MustGet(registry.ServiceKey).(registry.Servicer)
+	postsService := servicer.NewPosts()
+
+	posts, err := postsService.GetAll()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, NewErrorResponse("400", ErrorUnknown, err.Error()))
+		return
+	}
+	c.JSON(http.StatusCreated, posts)
+}
