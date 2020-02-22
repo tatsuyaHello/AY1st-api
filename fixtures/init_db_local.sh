689a192377126c263d8cd419d6f9dd0332a6b630
yamamaura_tatusya <t.yamamura@gemcook.com>
2020-02-22 15:11:29 +0900

    [update] Docker周り修正

--- fixtures/init_db_local.sh
+++ fixtures/init_db_local.sh
@@ -2,11 +2,11 @@
 
 echo "データベースのリセット実行開始"
 time (
-docker-compose exec mysql mysql -uroot -p --default-character-set=utf8 -e "DROP DATABASE IF EXISTS \`${DATABASE_NAME}\`;"
-docker-compose exec mysql mysql -uroot -p --default-character-set=utf8 -e "CREATE DATABASE \`${DATABASE_NAME}\` DEFAULT CHARACTER SET utf8;"
+docker-compose exec mysql mysql -uroot --default-character-set=utf8 -e "DROP DATABASE IF EXISTS \`${DATABASE_NAME}\`;"
+docker-compose exec mysql mysql -uroot --default-character-set=utf8 -e "CREATE DATABASE \`${DATABASE_NAME}\` DEFAULT CHARACTER SET utf8;"
 
 echo run sql file fixtures/db_schema.sql
-docker-compose exec mysql bash -c "mysql ${DATABASE_NAME} -uroot -p --default-character-set=utf8 < /var/fixtures/db_schema.sql"
+docker-compose exec mysql bash -c "mysql ${DATABASE_NAME} -uroot --default-character-set=utf8 < /var/fixtures/db_schema.sql"
 
 )
 echo "データベースのリセット実行完了"
