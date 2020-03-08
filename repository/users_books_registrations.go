676aa764b5ba16009510baa7ccf1f511cb9060a3
yamamaura_tatusya <t.yamamura@gemcook.com>
2020-03-08 17:52:31 +0900

    [update] バグ修正

--- repository/users_books_registrations.go
+++ repository/users_books_registrations.go
@@ -66,17 +66,17 @@ func (ubr *UsersBooksRegistrations) Create(userID, bookID uint64) (*model.UserBo
 		return nil, err
 	}
 
-	res := &model.UserBookRegistration{}
-	ok, err := ubr.engine.Where("user_id = ? AND book_id = ?", userID, bookID).Get(res)
-	if err != nil {
-		util.GetLogger().Error(err)
-		return nil, fmt.Errorf("can not get book")
-	}
-	if !ok {
-		return nil, fmt.Errorf("can not get book")
-	}
+	// res := &model.UserBookRegistration{}
+	// ok, err := ubr.engine.Where("user_id = ? AND book_id = ?", userID, bookID).Get(res)
+	// if err != nil {
+	// 	util.GetLogger().Error(err)
+	// 	return nil, fmt.Errorf("can not get book")
+	// }
+	// if !ok {
+	// 	return nil, fmt.Errorf("can not get book")
+	// }
 
-	return res, nil
+	return ubrs, nil
 }
 
 // GetOne は一意な投稿の結びつきを取得
